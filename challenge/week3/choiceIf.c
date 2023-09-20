b9f8098d27e2156cdfbbe7dba92cbe3806ae50c6
saine0501 <saine0501@gmail.com>
2023-09-20 15:57:25 +0900

    add week3 challenge

---
+++ challenge/week3/choiceIf.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+	int choice; // choice 변수 선언
+
+	printf("1. 파일 저장\n");
+	printf("2. 저장 없이 닫기\n");
+	printf("3. 종료\n");
+	scanf_s("%d", &choice); // 사용자로부터 choice 변수의 값 입력받기
+
+	// 사용자가 1을 입력하면 "파일을 저장합니다." 출력
+	if (choice == 1) { 
+		printf("파일을 저장합니다.");
+	}
+
+	// 사용자가 2를 입력하면 "저장 없이 닫습니다." 출력
+	else if (choice == 2) { 
+		printf("저장 없이 닫습니다.");
+	}
+
+	// 사용자가 1을 입력하면 "종료합니다." 출력
+	else if (choice == 3) { 
+		printf("종료합니다.");
+	}
+
+	// 사용자가 지정된 값이 아닌 다른 값을 입력하면 "잘못 입력하셨습니다." 출력
+	else {
+		printf("잘못 입력하셨습니다.");
+	}
+	return 0;
+}
---
+++ challenge/week3/zeroTwoIf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int num; // num 변수 선언
+	printf("숫자를 입력하시오: ");
+	scanf_s("%d", &num); // 사용자로부터 num 변수 값 입력받기
+
+	// 사용자가 0을 입력하면 "zero" 출력
+	if (num == 0) {
+		printf("zero");
+	}
+
+	// 사용자가 2를 입력하면 "one" 출력
+	else if (num == 1) {
+		printf("one");
+	}
+
+	// 사용자가 1을 입력하면 "two" 출력
+	else if (num == 2) {
+		printf("two");
+	}
+
+	// 0 ~ 2 범위가 아닌 다른 값을 입력하면 "not 0 ~ 2" 출력
+	else {
+		printf("not 0 ~ 2");
+	}
+	return 0;
+}
